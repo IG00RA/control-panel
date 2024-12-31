a9a50e1c59d77e971679451fd2754cd5dd38ead2
Ihor Hromadskiy <gromadskiyigor@gmail.com>
2024-12-31 14:05:08 +0200

    [Add] vacancy model

---
+++ types/generated/components.d.ts
@@ -0,0 +1,60 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface VacancyAdvantages extends Struct.ComponentSchema {
+  collectionName: 'components_vacancy_advantages';
+  info: {
+    description: '';
+    displayName: 'Advantages';
+    icon: 'shirt';
+  };
+  attributes: {
+    Advantage: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface VacancyRequirements extends Struct.ComponentSchema {
+  collectionName: 'components_vacancy_requirements';
+  info: {
+    description: '';
+    displayName: 'Requirements';
+    icon: 'feather';
+  };
+  attributes: {
+    Requirement: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface VacancyResponsibility extends Struct.ComponentSchema {
+  collectionName: 'components_vacancy_responsibilities';
+  info: {
+    description: '';
+    displayName: 'Responsibility';
+    icon: 'crown';
+  };
+  attributes: {
+    Responsibility: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface VacancySkill extends Struct.ComponentSchema {
+  collectionName: 'components_vacancy_skills';
+  info: {
+    description: '';
+    displayName: 'Skill';
+    icon: 'cloud';
+  };
+  attributes: {
+    Skill: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'vacancy.advantages': VacancyAdvantages;
+      'vacancy.requirements': VacancyRequirements;
+      'vacancy.responsibility': VacancyResponsibility;
+      'vacancy.skill': VacancySkill;
+    }
+  }
+}
